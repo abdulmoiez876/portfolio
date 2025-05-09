2b46b06a69ad295df927f08d8549825eaf9997e7
Abdul Moiez <74975190+abdulmoiez876@users.noreply.github.com>
2025-05-09 15:24:16 +0000

    Fix spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { LineShadowText } from "@/components/shadow-text";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-balance text-5xl font-semibold leading-none sm:text-6xl md:text-7xl lg:text-8xl">
-      Coming {' '}
-        <LineShadowText className="italic">
+    <div className="w-screen h-screen flex items-center justify-center text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+      Coming
+        <LineShadowText className="italic ml-4">
           Soon
         </LineShadowText>
     </div>
