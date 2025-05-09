642ae5cc936e1343e70893fa6812e08199011a88
Abdul Moiez <74975190+abdulmoiez876@users.noreply.github.com>
2025-05-09 15:17:51 +0000

    Remove tracking

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LineShadowText } from "@/components/shadow-text";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+    <div className="w-screen h-screen flex items-center justify-center text-balance text-5xl font-semibold leading-none sm:text-6xl md:text-7xl lg:text-8xl">
       Coming {' '}
         <LineShadowText className="italic">
           Soon
